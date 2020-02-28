94527c5e2957ba44c399e452b0e706c303b393a1
Santiago Torres G <thdealer@gmail.com>
2020-02-28 14:46:02 -0500

    Update store and add getAllMovies action

--- src/state/index.js
+++ src/state/index.js
@@ -1,4 +1,5 @@
 import { combineReducers } from 'redux';
 import app from './app';
+import movieReducer from './movieReducer';
 
-export default combineReducers({ app });
+export default combineReducers({ app, movies: movieReducer });
--- src/state/reduxWrapper.js
+++ src/state/reduxWrapper.js
@@ -1,9 +1,32 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { createStore as reduxCreateStore } from 'redux';
+import thunk from 'redux-thunk';
+import {
+  createStore as reduxCreateStore,
+  applyMiddleware,
+  compose,
+} from 'redux';
 import rootReducer from '.';
 
-const createStore = () => reduxCreateStore(rootReducer);
+const initState = {
+  movies: {
+    all: [],
+  },
+};
+
+const devTools =
+  typeof window === 'object' &&
+  typeof window.__REDUX_DEVTOOLS_EXTENSION__ !== 'undefined'
+    ? window.__REDUX_DEVTOOLS_EXTENSION__()
+    : f => f;
+
+const createStore = () =>
+  reduxCreateStore(
+    rootReducer,
+    initState,
+    compose(applyMiddleware(thunk), devTools),
+  );
+
 export default ({ element }) => (
   <Provider store={createStore()}>{element}</Provider>
 );
