9035e1a0d8293c3d614284b83d47ac63dfcf313c
Santiago Torres G <thdealer@gmail.com>
2020-02-28 14:47:28 -0500

    Update component to get data from the API

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,31 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { connect, useDispatch } from 'react-redux';
 import { Link } from 'gatsby';
 import Layout from '../components/layout';
 import SEO from '../components/seo';
+import { getAllMovies } from '../state/movieActions';
+const IndexPage = ({ movies }) => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getAllMovies());
+  }, [dispatch]);
+  console.log('movies', movies);
 
-const IndexPage = () => (
-  <Layout>
-    <SEO
-      title="Películas"
-      keywords={['cinema', 'booking', 'films', 'reserva', 'cine', 'films']}
-    />
-    <h1>Películas</h1>
-  </Layout>
-);
+  return (
+    <Layout>
+      <SEO
+        title="Películas"
+        keywords={['cinema', 'booking', 'films', 'reserva', 'cine', 'films']}
+      />
+      <h1>Películas</h1>
+      {movies.length > 0
+        ? movies.map(post => <pre>{JSON.stringify(post, null, 2)}</pre>)
+        : 'loading'}
+    </Layout>
+  );
+};
 
-export default IndexPage;
+const mapStateToProps = state => ({
+  movies: state.movies.all,
+});
+export default connect(mapStateToProps, null)(IndexPage);
