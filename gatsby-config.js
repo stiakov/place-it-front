4a2b7ec77291381565f56a5632460bcb7de4741e
Santiago Torres G <thdealer@gmail.com>
2020-02-28 12:49:59 -0500

    Update favicon and gatsby config file

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `PlaceIt`,
+    description: `Daily Movies, Immediate bookings.`,
+    author: `Santiago Torres G - @stiakov`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -24,7 +24,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/favicon.ico`, // This path is relative to the root of the site.
       },
     },
     'gatsby-plugin-emotion',
