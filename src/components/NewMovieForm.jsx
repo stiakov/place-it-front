7a36e368f38133255acc49f0305b62db7749e79e
Santiago Torres G <thdealer@gmail.com>
2020-02-29 22:49:23 -0500

    Refactor Form to avoid incompatibilities with netlify

--- src/components/NewMovieForm.jsx
+++ src/components/NewMovieForm.jsx
@@ -1,13 +1,7 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { css } from '@emotion/core';
-import {
-  Form,
-  TextField,
-  SubmitField,
-  CheckboxField,
-} from 'react-components-form';
-import Schema from 'form-schema-validation';
+import { Form, Field } from 'react-final-form';
 import { DateRange } from 'react-date-range';
 import { createMovie, setShowNewMovieModal } from '../state/movieActions';
 import {
@@ -27,98 +21,94 @@ const NewMovieForm = () => {
     },
   ]);
 
-  const ErrorMessages = {
-    validateRequired(key) {
-      return `'${key}' requerido`;
-    },
-  };
-  const createMovieSchema = new Schema(
-    {
-      title: {
-        type: String,
-        required: true,
-      },
-      plot: {
-        type: String,
-        required: true,
-      },
-      poster: {
-        type: String,
-        required: true,
-      },
-      Confirmar: {
-        type: Boolean,
-        required: true,
-      },
-    },
-    ErrorMessages,
-  );
-
   const submitCreation = data => dispatch(createMovie(data));
-
   const cancelCreation = () => dispatch(setShowNewMovieModal(false));
+  const required = value => (value ? undefined : 'Requerido');
 
   return (
     <Form
-      schema={createMovieSchema}
-      onSubmit={model => submitCreation({ ...model, dates })}
-      onError={(errors, data) => console.log('error', errors, data)}
-    >
-      <h3>Crear película</h3>
-      <div>
-        <TextField
-          name="title"
-          label="Título"
-          type="text"
-          placeholder="Ingrese el título"
-        />
-        <TextField
-          name="plot"
-          label="Sinopsis"
-          type="text"
-          placeholder="Ingrese la sinopsis"
-        />
-        <TextField
-          name="poster"
-          label="Poster"
-          type="text"
-          placeholder="Ingrese la url del poster"
-        />
-      </div>
-
-      <div>
-        <label
-          css={css`
-            margin-top: 0.7rem;
-          `}
-        >
-          Seleccione las fechas
-        </label>
-        <DateRange
-          css={css`
-            margin-top: 0.7rem;
-          `}
-          editableDateInputs={true}
-          onChange={item => setDates([item.selection])}
-          moveRangeOnFirstSelection={false}
-          ranges={dates}
-        />
-      </div>
-      <div css={blockStyles}>
-        <CheckboxField name="Confirmar" />
-        <label htmlFor="Confirmar">Confirmo que los datos son correctos</label>
-      </div>
-      <div css={blockStyles}>
-        <SubmitField css={addButtonStyles} value="┼ &nbsp; Guardar" />
-        <button
-          css={cancelButtonStyles}
-          type="cancel"
-          onClick={() => cancelCreation()}
-        >
-          Cancelar
-        </button>
-      </div>
-    </Form>
+      onSubmit={values => submitCreation({ ...values, dates })}
+      render={({ handleSubmit, form, submitting, pristine, values }) => (
+        <form onSubmit={handleSubmit}>
+          <Field name="title" validate={required}>
+            {({ input, meta }) => (
+              <div>
+                <label>Título</label>
+                <input {...input} type="text" placeholder="Ingrese el título" />
+                {meta.error && meta.touched && <span>{meta.error}</span>}
+              </div>
+            )}
+          </Field>
+          <Field name="plot" validate={required}>
+            {({ input, meta }) => (
+              <div>
+                <label>Sinopsis</label>
+                <input
+                  {...input}
+                  type="text"
+                  placeholder="Ingrese la sinopsis"
+                />
+                {meta.error && meta.touched && <span>{meta.error}</span>}
+              </div>
+            )}
+          </Field>
+          <Field name="poster" validate={required}>
+            {({ input, meta }) => (
+              <div>
+                <label>Poster</label>
+                <input
+                  {...input}
+                  type="text"
+                  placeholder="Ingrese la url de la imagen"
+                />
+                {meta.error && meta.touched && <span>{meta.error}</span>}
+              </div>
+            )}
+          </Field>
+          <label
+            css={css`
+              margin-top: 0.7rem;
+            `}
+          >
+            Seleccione las fechas
+          </label>
+          <DateRange
+            css={css`
+              margin-top: 0.7rem;
+            `}
+            editableDateInputs={true}
+            onChange={item => setDates([item.selection])}
+            moveRangeOnFirstSelection={false}
+            ranges={dates}
+          />
+          <div css={blockStyles}>
+            <Field
+              name="Confirmar"
+              component="input"
+              type="checkbox"
+              validate={required}
+            />
+            <label htmlFor="Confirmar">
+              Confirmo que los datos son correctos
+            </label>
+          </div>
+          <div css={blockStyles} className="buttons">
+            <button type="submit" css={addButtonStyles}>
+              ┼ &nbsp; Guardar
+            </button>
+            <button
+              css={cancelButtonStyles}
+              type="cancel"
+              onClick={() => cancelCreation()}
+              type="button"
+            >
+              Cancelar
+            </button>
+          </div>
+          {/* <pre>{JSON.stringify({ ...values, dates }, 0, 2)}</pre> */}
+        </form>
+      )}
+    />
   );
 };
 
