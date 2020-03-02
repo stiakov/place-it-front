1789f6c76b12f4bab21ded313e1e44695472895e
Santiago Torres G <thdealer@gmail.com>
2020-03-02 18:42:38 -0500

    Improve User experience with css spinners

--- src/components/indexMovies/NewReservationForm.jsx
+++ src/components/indexMovies/NewReservationForm.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { connect, useDispatch } from 'react-redux';
 import { Form, Field } from 'react-final-form';
 import Loading from '../Loading';
